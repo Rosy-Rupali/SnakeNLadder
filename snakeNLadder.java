bcb21e5978521972fef7e7ee7e5f60d2492fe8c9
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-01 19:56:33 +0530

    initializing variable

--- snakeNLadder.java
+++ snakeNLadder.java
@@ -1,8 +1,7 @@
 package snakeandladder;
 
 public class snakeNLadder {
-	public static void main(String[] args) {
-		System.out.println("Snake and Ladder problem");
-	}
+	
+	int playerPosition = 0;
 	
 }

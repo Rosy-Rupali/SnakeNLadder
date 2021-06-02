37588c2ee8677dfa4300455984f4e28e2750428f
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-02 11:40:18 +0530

    added: played game with two players

--- snakeNLadder.java
+++ snakeNLadder.java
@@ -15,20 +15,34 @@ public  class snakeNLadder {
 	}
 	
 	public void startGame() {
-		int playerPosition = 0, diceValue=0;
-		int playerDiceCount=0;
+		int player1Position = 0, diceValue=0, player1DiceCount=0;
+		int player2Position = 0, player2DiceCount=0;
+		int currentPlayer = -1;
 		while(true) {
 			diceValue = rollDie();
-			playerDiceCount++;
-			playerPosition = calculatePlayerPosition(playerDiceCount, diceValue);
-			System.out.println("Player position: "+playerPosition);
-			if(playerPosition == WIN_POINT) {
-				System.out.println("Player wins the game");
-				System.out.println("Die rolled: "+playerDiceCount);
+			if(currentPlayer == -1) {
+			player1DiceCount++;
+			player1Position = calculatePlayerPosition(player1DiceCount, diceValue);
+			System.out.println("Player1 position: "+player1Position);
+			if(player1Position == WIN_POINT) {
+				System.out.println("Player1 wins the game");
+				System.out.println("Die rolled: "+player1DiceCount);
 				break;
 			}
 		}
+		else {
+			player2DiceCount++;
+			player2Position = calculatePlayerPosition(player2DiceCount, diceValue);
+			System.out.println("Player2 position: "+player2Position);
+			if(player2Position == WIN_POINT) {
+				System.out.println("Player2 wins the game");
+				System.out.println("Die rolled: "+player2DiceCount);
+				break;
+			}
+		}
+			currentPlayer = -(currentPlayer);
 	}
+}
 	
 	public int rollDie() {
 		return random.nextInt(6)+1;

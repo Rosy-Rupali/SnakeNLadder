1a199173af6b54dd1e3f8bbc9b89d989d4e56e31
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-02 11:25:52 +0530

    added: DiceCount and player position

--- snakeNLadder.java
+++ snakeNLadder.java
@@ -5,8 +5,30 @@ import java.util.Random;
 public  class snakeNLadder {
 	
 	Random random = new Random();
-	int player = 0;
+	
 	int WIN_POINT=100;
+	
+	public static void main(String[] args) {
+		
+		snakeNLadder sn = new snakeNLadder();
+		sn.startGame();
+	}
+	
+	public void startGame() {
+		int playerPosition = 0, diceValue=0;
+		int playerDiceCount=0;
+		while(true) {
+			diceValue = rollDie();
+			playerDiceCount++;
+			playerPosition = calculatePlayerPosition(playerDiceCount, diceValue);
+			System.out.println("Player position: "+playerPosition);
+			if(playerPosition == WIN_POINT) {
+				System.out.println("Player wins the game");
+				System.out.println("Die rolled: "+playerDiceCount);
+				break;
+			}
+		}
+	}
 	
 	public int rollDie() {
 		return random.nextInt(6)+1;
